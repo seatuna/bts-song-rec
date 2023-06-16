14e4a9f609b1177cfbd83dfc8b890dcbe3307783
Celena T <chibiynm@gmail.com>
2023-06-15 22:39:57 -0400

    add 404 error, use flask jsonify

--- api/app.py
+++ api/app.py
@@ -1,6 +1,5 @@
 import configparser
-from flask import Flask
-import json
+from flask import Flask, jsonify, abort
 import numpy as np
 import pandas as pd
 
@@ -12,13 +11,24 @@ PORT = CONFIG["dev"]["port"]
 app = Flask(__name__)
 
 
+@app.errorhandler(404)
+def not_found(e):
+    """Return generic 404 error"""
+    return jsonify(error=str(e)), 404
+
+
 @app.route("/song/<string:song_id>", methods=["GET"])
 def get_song_data(song_id):
     """Get song"""
     bts_songs = pd.read_csv("data/bts-songs-names-and-features-spotify.csv")
     song = bts_songs.loc[bts_songs["spotify_id"] == song_id]
+
+    if song.empty:
+        abort(404, description="Song not found")
+
+    print(song)
     print(song.index[0])
-    return json.loads(song.reset_index().to_json(orient="records"))[0]
+    return jsonify(song.reset_index().to_dict(orient="records")[0])
 
 
 @app.route("/song/<string:song_id>/similar", methods=["GET"])
@@ -26,10 +36,14 @@ def get_similar_songs(song_id):
     """Get first five similar songs"""
     bts_songs = pd.read_csv("data/bts-songs-names-and-features-spotify.csv")
     song = bts_songs.loc[bts_songs["spotify_id"] == song_id]
-    index = song.index[0]
 
+    if song.empty:
+        abort(404, description="Song not found")
+
+    index = song.index[0]
     cos_sim = np.load("data/bts-song-cos-sim.npy")
     similar_songs = list(enumerate(cos_sim[index]))
+
     # First 5 similar songs
     sorted_similar_songs = sorted(similar_songs, key=lambda x: x[1], reverse=True)[1:6]
     index_list = [value[0] for value in sorted_similar_songs]
@@ -37,7 +51,7 @@ def get_similar_songs(song_id):
     # Retrieve songs from songs df
     similar_songs_df = bts_songs.iloc[index_list]
 
-    return json.loads(similar_songs_df.reset_index().to_json(orient="records"))
+    return jsonify(similar_songs_df.reset_index().to_dict(orient="records"))
 
 
 if __name__ == "__main__":
