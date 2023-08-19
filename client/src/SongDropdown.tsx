d412d33faba37258da974d4b2f4314d25b628d16
Celena T <chibiynm@gmail.com>
2023-08-19 16:09:23 -0400

    add cors config, fix type errors and resolve warnings

--- client/src/SongDropdown.tsx
+++ client/src/SongDropdown.tsx
@@ -1,19 +1,26 @@
 import useSWR from "swr";
 import { useDebounce } from "./hooks/useDebounce";
 import { fetcher } from "./utils";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, Fragment, useState } from "react";
+
+interface Song {
+  spotify_id: string;
+  name: string;
+}
 
 export const SongDropdown = () => {
   const [searchQuery, setSearchQuery] = useState<string>("");
-  const debouncedSearchQuery = useDebounce(searchQuery);
+  const debouncedSearchQuery = useDebounce(searchQuery) as string;
   const queryParams = new URLSearchParams({ query: debouncedSearchQuery });
-  const { data, error, isLoading } = useSWR(
-    `http://localhost:5000/song/search?${queryParams}}`,
+  const { data, error, isLoading } = useSWR<Song[]>(
+    queryParams && debouncedSearchQuery
+      ? `http://localhost:5000/song/search?${queryParams}`
+      : null,
     fetcher
   );
 
   const handleSearch = (event: ChangeEvent<HTMLInputElement>): void => {
-    console.log("event target:", event.target.value);
+    event.preventDefault();
     setSearchQuery(event.target.value);
   };
 
@@ -43,9 +50,9 @@ export const SongDropdown = () => {
               xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                clip-rule="evenodd"
+                clipRule="evenodd"
               ></path>
             </svg>
           </div>
@@ -70,9 +77,9 @@ export const SongDropdown = () => {
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
             ></path>
           </svg>
@@ -81,11 +88,11 @@ export const SongDropdown = () => {
       </form>
 
       {data && (
-        <div>
-          {data.map(() => {
-            return <div>{data.name}</div>;
+        <Fragment>
+          {data.map((song: Song, i: number) => {
+            return <div key={i}>{song.name}</div>;
           })}
-        </div>
+        </Fragment>
       )}
     </>
   );
